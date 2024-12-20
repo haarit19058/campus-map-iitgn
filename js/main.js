c9d908c070f7b57a0d9ed983ae0026ffa2ff833d
Haarit <haarit1905@gmail.com>
2024-12-20 12:01:49 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,72 @@
+const map = L.map('map').setView([23.2108, 72.6877], 18);
+
+
+ // Add OpenStreetMap tiles
+ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
+const locations = [
+    { 
+        name: 'Library', 
+        coords: [23.2150, 72.6839], 
+        info: 'Central Library of IITGN', 
+        image: 'https://example.com/library.jpg' 
+    },
+    { 
+        name: 'Boys Hostel Block A', 
+        coords: [23.2139, 72.6851], 
+        info: 'Hostel for undergraduate students', 
+        image: 'https://example.com/boys_hostel.jpg' 
+    },
+    { 
+        name: 'Girls Hostel', 
+        coords: [23.2148, 72.6862], 
+        info: 'Accommodation for female students', 
+        image: 'https://example.com/girls_hostel.jpg' 
+    },
+    { 
+        name: 'Main Academic Block', 
+        coords: [23.2146, 72.6834], 
+        info: 'Academic hub of the institute', 
+        image: 'https://example.com/academic_block.jpg' 
+    },
+    { 
+        name: 'Sports Complex', 
+        coords: [23.2135, 72.6820], 
+        info: 'Sports facilities including a gym and courts', 
+        image: 'https://example.com/sports_complex.jpg' 
+    }
+];
+
+// Add markers with popups
+locations.forEach(loc => {
+    L.marker(loc.coords)
+        .addTo(map)
+        .bindPopup(`
+            <div class="popup-content">
+                <img src="${loc.image}" alt="${loc.name}">
+                <b>${loc.name}</b><br>
+                ${loc.info}
+            </div>
+        `);
+});
+
+// Search functionality
+document.getElementById('search').addEventListener('input', (e) => {
+    const query = e.target.value.toLowerCase();
+    const location = locations.find(loc => loc.name.toLowerCase().includes(query));
+    if (location) {
+        map.setView(location.coords, 18); // Zoom to the location
+        L.popup()
+            .setLatLng(location.coords)
+            .setContent(`
+                <div class="popup-content">
+                    <img src="${location.image}" alt="${location.name}">
+                    <b>${location.name}</b><br>
+                    ${location.info}
+                </div>
+            `)
+            .openOn(map);
+    }
+});
